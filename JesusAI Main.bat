5dd6238923df3fec4e01d5cdf7971d07cc24d232
Jesus <96124233+JesusAIexperience@users.noreply.github.com>
2021-12-15 23:37:49 +0000

    Added main JesusAI build

---
+++ JesusAI Main.bat
@@ -0,0 +1,111 @@
+@echo off
+title Jesus A.I
+:menu
+color 0a
+cls
+echo Jesus A.I 
+echo Please pick an option
+echo 1. Start Experience
+echo 2. Exit
+set /p menu=
+if '%menu%'=='1' (
+goto jesusai
+)
+if '%menu%'=='3' (
+cls
+color 3c
+echo THE DAY OF JUDGEMENT IS FAST APPROACHING AND YOU WILL BE HARSHLY JUDGED IF YOU
+echo CONTINUE TO DISOBEY ME 
+color 1a
+pause
+exit 
+)
+if '%menu%'=='2' (
+exit
+)else goto menu
+:jesusai
+cls 
+color 0c
+echo                            ,($$ @  @(/  / $ ./.                                
+echo                         (@                       $@                           
+echo                     .$                               $                        
+echo                   $$                 .@                 $                      
+echo                 /                 .@. .@                ,                    
+echo                /              ,$@(       ,$/             (                    
+echo                @        $@$(.                $@$ .//      @                   
+echo               $,       @$                           ,@      (                  
+echo               @       $.                              $(    .                
+echo              $     $$                                  @    (                
+echo             $    $/                                   $     @                
+echo            .$       @    ,(@@$/,$          ,$,/$$$    ./     @                
+echo           $(        $.     ($@               $$$,      @      @                
+echo          $/         .$    ,@@@$.     .      ($$$(.    (       $                
+echo          @.          @               /                $       $                
+echo          $/          @.              ,                @       /.               
+echo           $          $.                               $        ((              
+echo            @        .@             .    ,             ,/        $$             
+echo            $,       $@,             .@$(             (/$          @            
+echo            @        $$ @     __.$$ #     #$$$__    $$ ,($         ,$           
+echo         $@.         @$  \ #/$@/@     #$$#/$#    #$/  $(./           $          
+echo        @            @ $         (@. ~~~~~  $,         ,$           ,@          
+echo       /(            ,.$       ( @    $$$    $ .      //            ,@          
+echo       $,             .,.         $$$    $/$/        $              $          
+echo       @            ,   $,                         $@              $@           
+echo       $            ,    @$@                     $,@ @.           $            
+echo        $             $$@@@$(                     $ ($/            /           
+echo         @.             @.@  $                   $  ,$@ .(         @.          
+echo          ,$            $$@     @              $.   ,$ @@         /$        
+echo          $ $,          $. $     .@.         /$     $$ /$        $$       
+echo            @$         .@  $,       @    .@/        @  $$      $(               
+echo           @         ($     @        .(@.@         ,$  ,$     @                 
+echo          /(       ,/ $,    .@          $          $.   @      $@.              
+echo           @       .   ,$    $@                    @     $        $             
+echo            @           .(    ,$                  $$       $/     @             
+echo             (@          @$    @                 .@        $@,   ,$             
+echo               .@       ,, $@$.($                @    .$@(  $,  .@,             
+echo                 //    .(      ,$@@@$$/,.     ,($@@@$,      @  $@.              
+echo                  @  .@.                                  @. .@@                
+echo                 ,/$.                                    $  @, @                                   
+echo (your text) ASK JESUS A QUESTION
+
+echo 2. BEG FOR FORGIVENESS
+
+echo 3. ESCAPE
+set /p jesusai=
+if '%jesusai%'=='2' (
+goto hell
+)
+if '%jesusai%'=='3' (
+goto error
+ color 0a
+:error
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%   
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
+goto error
+) 
+::here a poopoo ton of code shall begin 
+if '%jesusai%'=='hello' (
+goto %hello%
+)
+
+:hello
+cls 
+echo Hello. A%%%%%@@£$£$"%"^NYThING else you would like to ask my disciple?
+
+echo                 I
+echo                 I
+echo                 I
+echo                 I
+echo       __________I___________
+echo                 I
+echo                 I
+echo                 I
+echo                 I
+echo                 I
+echo                 I
+echo                 I
+echo                 I
+echo                 I
+echo                 I
+echo 
+pause
