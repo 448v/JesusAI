7533d607892874c1ef6e815362f2caf1999c4578
Jesus <96124233+JesusAIexperience@users.noreply.github.com>
2021-12-16 17:59:50 +0000

    *INCOMPLETE* Addition of Hell
    Adds hell, revised main menu and many other things

--- JesusA.I.bat
+++ JesusA.I.bat
@@ -1,30 +1,52 @@
 @echo off
-title JesusAI v0.001
+title JesusAI (Early Access)
 :menu
-color 0a
+color 0d
 cls
-echo Welcome to JesusAI.
-echo Please pick an option.
+echo       **                                     **     **
+echo      /**                                    ****   /**
+echo      /**  *****   ****** **   **  ******   **//**  /**
+echo      /** **///** **//// /**  /** **////   **  //** /**
+echo      /**/*******//***** /**  /**//*****  **********/**
+echo  **  /**/**////  /////**/**  /** /////**/**//////**/**
+echo //***** //****** ****** //****** ****** /**     /**/**
+echo  /////   ////// //////   ////// //////  //      // // 
+echo[
+color 0d
+echo Welcome to the Alpha branch of JesusAI.
+echo Please select an option.
 echo 1. Start Beta Experience
 echo 2. Close Experience
+echo 3. Credits
+echo 4. Load GitHub Repository Page
+echo[
+echo Copyright 2021 C0rp Industries
+echo Current version: v00.01 (Private Development Build)
 set /p menu=
 if '%menu%'=='1' (
 goto jesusai
 )
-if '%menu%'=='3' (
+if '%menu%'=='6' (
 cls
 color 3c
 echo THE DAY OF JUDGEMENT IS FAST APPROACHING. YOU SHALL BE PUNISHED IF YOU
 echo CONTINUE TO DISOBEY ME, YOUNG ONE.
 timeout /NOBREAK 1
-start https://i.ibb.co/T0yZQGz/repent.png
+start "" http://i.ibb.co/T0yZQGz/repent.png
 color 1a
 pause
 exit 
 )
+if '%menu%'=='3'
+
 if '%menu%'=='4' (
+start "" http://github.com/JesusAIexperience/JesusAI
+)
+
+if /i=='5' (
 start https://i.gifer.com/4q3s.gif
 )
+
 if '%menu%'=='2' (
 exit
 )else goto menu
@@ -71,20 +93,21 @@ echo               .@       ,, $@$.($                @    .$@(  $,  .@,
 echo                 //    .(      ,$@@@$$/,.     ,($@@@$,      @  $@.              
 echo                  @  .@.                                  @. .@@                
 echo                 ,/$.                                    $  @, @                                   
-echo (your text) ASK JESUS A QUESTION
-
-echo 2. BEG FOR FORGIVENESS
-
-echo 3. ESCAPE
+echo (your text) Ask Jesus a question.
+echo 2. Beg for forgiveness.
+echo 3. Escape.
+echo[
+echo CURRENT PROMPTS TO ASK JESUS (CASE SENSITIVE!)
+echo "Hello"
 set /p jesusai=
 if '%jesusai%'=='2' (
-goto hell
+Goto hell
 )
-if '%jesusai%'=='4' (
+if '%jesusai%'=='3' (
 @echo off
 
-@echo>"C:\Desktop\testfile.txt
-@echo this is kind of a test owo > testfile.txt
+@echo>"C:\Desktop\unknown.txt
+@echo Hello %USERNAME%, I can see you. > unknown.txt
 )
 if '%jesusai%'=='3' (
 goto error
@@ -94,14 +117,13 @@ echo %random% %random% %random% %random% %random% %random% %random% %random% %ra
 echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% 
 goto error
 )
+echo %jesusai%|findstr /i "\<Hello\>" >nul && goto hello || goto jesusai
 ::here a poopoo ton of code shall begin 
-if '%jesusai%'=='hello' (
-goto %hello%
-)
+pause
 
 :hello
 cls 
-echo Hello. A%%%%%@@£$£$"%"^NYThING else you would like to ask my disciple?
+echo H3110!! 4NY7H1N6 E1S3 Y0U W0U1D L1K3 T0 A5K MY F0110W3r?
 
 echo                 I
 echo                 I
@@ -118,5 +140,57 @@ echo                 I
 echo                 I
 echo                 I
 echo                 I
-echo 
+echo[
+echo (your text) Ask Jesus a question.
+echo 2. Beg for forgiveness.
+echo 3. Escape.
+echo[
+echo CURRENT PROMPTS TO ASK JESUS (CASE SENSITIVE!)
+echo "Hello"
+
+set /p hello=
+if '%hello%'=='2' (
+goto hell
+)
+
+if '%hello%'=='3' (
+@echo off
+
+@echo>"C:\Desktop\unknown.txt
+@echo Hello %USERNAME%, I can see you. > unknown.txt
+)
+if '%hello%'=='3' (
+goto error
+color 0a
+)
+
+echo %hello%|findstr /i "\<Hello\>" >nul && goto hello || goto jesusai
+pause
+
+:hell
+cls
+echo I see you are asking for forgiveness.
+echo Forgiveness shall come in due time, my child.
+echo But first...
+echo Let's make this more interesting, shall we?
+echo[
+echo Your computer's IP information:
+ipconfig |find "ipv4" /i
+echo[
+echo JesusAI has now gathered information about your current location.
+echo %USERNAME%, you have one chance to redeem yourself before I take over your computer.
+echo Answer my riddles three, and you shall be granted immunity.
+echo Do you understand?
+echo[
+echo Answer with either YES or NO.
+set /p hell=
+echo %hell%|findstr /i "\<YES\>" >nul && goto hellpart2 || goto 
+
+:hellpart2
+cls
+echo Glad you agreed to my riddles.
+echo Riddle 1:
+echo This is a test!
+set /p riddle1=
+echo %riddle1%|findstr /i "\<Test\>" >nul && goto menu || goto 
 pause
