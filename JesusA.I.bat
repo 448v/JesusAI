89f4d59d9eb8f1d0edf18a7f3c109dc3b70f59f3
Max <96433729+schneerocket@users.noreply.github.com>
2021-12-22 21:29:07 +0000

    Removed valuable asset retrieval

--- JesusA.I.bat
+++ JesusA.I.bat
@@ -1,7 +1,6 @@
-::Alpha branch created by @ScoopyScrap
-::Very optimized
-::Works in Powershell, Command Prompt or ran from the directory 
-::20/12/21: Command Prompt may be unstable and crash more than Powershell
+::Alpha branch coded and maintained by @schneerocket on GitHub and Instagram
+::JesusAI runs best on Powershell, however it will load using Command Prompt too
+::Found an issue? Report it at our repository: https://github.com/JesusAIexperience/JesusAI
 
 
 @echo off
@@ -22,7 +21,7 @@ echo[
 color 0a
 echo Welcome to the Alpha build of JesusAI.
 echo Please select an option.
-echo 1. Start The Experience
+echo 1. Start Experience
 echo 2. Close Experience
 echo 3. Load GitHub Repository Page
 echo 4. Credits
@@ -32,9 +31,7 @@ echo Current version: v00.01 (Private Development Build)
 echo[
 echo This version may be unstable and is susceptible to crashing. You've been warned!
 echo[
-
 set /p "menu2=Selected option: "
-if "%menu2%" equ "enable.dirchecker" goto dice
 if "%menu2%" equ "enable.debug" goto debugmenu
 if "%menu2%" equ "secret" goto judgement
 if "%menu2%" equ "jesus" start "" https://i.imgur.com/DCtv3fR.gif
@@ -43,36 +40,6 @@ if "%menu2%" equ "3" start "" "https://github.com/JesusAIexperience/JesusAI"
 if "%menu2%" equ "2" exit
 if "%menu2%" equ "1" goto jesusai
 goto menu
-
-:dice
-cls
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo %random%  %random%  %random%
-echo[
-echo Match dir?
-echo[
-set /p "dice=?: "
-if "%dice%" equ "true" goto confirm
-if "%dice%" equ "false" goto menu
-goto dice
-
-:confirm
-cls
-echo Maximised output was detected in JesusAI. Signal has been confirmed. >>"%~dp0dirResults.txt"
-echo Confirmed
-echo[
-set /p "accept=?: "
-if "%accept%" equ "accept" goto menu
-exit
 
 :debugmenu
 color 0a
@@ -92,7 +59,7 @@ echo Enter "disable.debug" to disable debug options.
 echo[
 echo Welcome to the Alpha build of JesusAI.
 echo Please select an option.
-echo 1. Start The Experience
+echo 1. Start Experience
 echo 2. Close Experience
 echo 3. Load GitHub Repository Page
 echo 4. Credits
@@ -110,7 +77,6 @@ echo[
 ::DON'T MESS AROUND WITH THIS IF YOU DON'T KNOW WHAT TO FUCKING DO
 
 set /p "menudebug=Selected option: "
-if "%menudebug%" equ "enable.dirchecker" goto dice
 if "%menudebug%" equ "disable.debug" goto menu
 if "%menudebug%" equ "secret" goto judgement
 if "%menudebug%" equ "jesus" start "" https://i.imgur.com/DCtv3fR.gif
